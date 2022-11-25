2d2f8fec325aa2a04fc2882f6731b14dd2e9a4e9
Vitriol1744 <vitriol1744@gmail.com>
2022-11-25 20:14:17 +0100

    Use templates for string to int conversion and vice versa

--- Kernel/KLibC.cpp
+++ Kernel/KLibC.cpp
@@ -1,18 +1,5 @@
 #include "KLibC.hpp"
 
-int pow(int num, int power)
-{
-    int invert  = 0;
-
-    int powMult = 1;
-
-    if ((invert = (power < 0))) power = -power;
-
-    for (int x = 0; x < power; x++) powMult *= num;
-
-    return (invert) ? 1 / powMult : powMult;
-}
-
 extern "C" size_t strlen(const char* str)
 {
     size_t length = 0;
@@ -65,60 +52,4 @@ extern "C" void* memcpy(void* dest, const void* src, size_t bytes)
         --bytes;
     }
     return dest;
-}
-
-int atoi(const char* str) { atoi(str, strlen(str)); }
-int atoi(const char* str, size_t length)
-{
-    int    integer      = 0;
-    bool   isNegative   = str[0] == '-';
-
-    size_t index        = isNegative;
-    size_t stringLength = length, power = stringLength - isNegative;
-
-    for (; index < stringLength; index++)
-        integer += (str[index] - 48) * pow(10, --power);
-
-    return (isNegative) ? -integer : integer;
-}
-char* itoa(int32_t value, char* str, int base)
-{
-    int  i          = 0;
-    bool isNegative = false;
-
-    if (value == 0)
-    {
-        str[i++] = '0';
-        str[i]   = 0;
-        return str;
-    }
-
-    if (value < 0 && base == 10)
-    {
-        isNegative = true;
-        value      = -value;
-    }
-
-    while (value != '\0')
-    {
-        int rem  = value % base;
-        str[i++] = (rem > 9) ? (char)((rem - 10) + 'a') : (char)(rem + '0');
-        value    = value / base;
-    }
-
-    if (isNegative) str[i++] = '-';
-    str[i]    = '\0';
-
-    int start = 0;
-    int end   = i - 1;
-    while (start < end)
-    {
-        char c         = *(str + start);
-        *(str + start) = *(str + end);
-        *(str + end)   = c;
-        start++;
-        end--;
-    }
-
-    return str;
 }
--- Kernel/KLibC.hpp
+++ Kernel/KLibC.hpp
@@ -13,6 +13,79 @@ extern "C"
     void*  memcpy(void* dest, const void* src, size_t bytes);
 }
 
-int   atoi(const char* str);
-int   atoi(const char* str, size_t length);
-char* itoa(int32_t value, char* str, int base);
+template <typename T>
+T pow(T num, T power)
+{
+    T invert  = 0;
+
+    T powMult = 1;
+
+    if ((invert = (power < 0))) power = -power;
+
+    for (T x = 0; x < power; x++) powMult *= num;
+
+    return (invert) ? 1 / powMult : powMult;
+}
+
+template <typename T>
+T atoi(const char* str, size_t length)
+{
+    T      integer      = 0;
+    bool   isNegative   = str[0] == '-';
+
+    size_t index        = isNegative;
+    size_t stringLength = length, power = stringLength - isNegative;
+
+    for (; index < stringLength; index++)
+        integer += (str[index] - 48) * pow<T>(10, --power);
+
+    return (isNegative) ? -integer : integer;
+}
+template <typename T>
+T atoi(const char* str)
+{
+    return atoi<T>(str, strlen(str));
+}
+
+template <typename T>
+char* itoa(T value, char* str, int base)
+{
+    T    i          = 0;
+    bool isNegative = false;
+
+    if (value == 0)
+    {
+        str[i++] = '0';
+        str[i]   = 0;
+        return str;
+    }
+
+    if (value < 0 && base == 10)
+    {
+        isNegative = true;
+        value      = -value;
+    }
+
+    while (value != '\0')
+    {
+        T rem    = value % base;
+        str[i++] = (rem > 9) ? (char)((rem - 10) + 'a') : (char)(rem + '0');
+        value    = value / base;
+    }
+
+    if (isNegative) str[i++] = '-';
+    str[i]  = '\0';
+
+    T start = 0;
+    T end   = i - 1;
+    while (start < end)
+    {
+        char c         = *(str + start);
+        *(str + start) = *(str + end);
+        *(str + end)   = c;
+        start++;
+        end--;
+    }
+
+    return str;
+}
