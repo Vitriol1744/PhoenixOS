bf879d69b6f53ba8f67478e2afd174fdfba133d1
Vitriol1744 <vitriol1744@gmail.com>
2022-12-02 20:18:44 +0100

    Fix Bitmap

---
+++ Kernel/Utility/Bitmap.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Common.hpp"
+
+#include "KLibC.hpp"
+
+struct Bitmap
+{
+    Bitmap() = default;
+    Bitmap(uint8_t* data, const size_t size, uint8_t value = 1)
+        : data(data)
+        , size(size)
+    {
+        SetAll(value);
+    }
+
+    inline void SetAll(uint8_t value) { memset(data, value, size); }
+    inline void SetIndex(uint64_t index, bool value)
+    {
+        const uint64_t byte = index / 8;
+        const uint64_t bit  = index % 8;
+
+        if (value) data[byte] |= BIT(bit);
+        else data[byte] &= ~BIT(bit);
+    }
+    inline bool GetIndex(uint64_t index)
+    {
+        const uint64_t byte = index / 8;
+        const uint64_t bit  = index % 8;
+
+        return data[byte] & BIT(bit);
+    }
+
+    uint8_t* data;
+    size_t   size;
+};
