0fa09040480ea588d1ff1d65d74214c24c8b4245
Vitriol1744 <vitriol1744@gmail.com>
2022-12-07 22:31:39 +0100

    Cleanup

--- Kernel/BootInfo.cpp
+++ Kernel/BootInfo.cpp
@@ -4,61 +4,60 @@
 
 #include "limine.h"
 
-static constexpr const uint32_t DEFAULT_STACK_SIZE = 65536;
+#include <stdatomic.h>
 
-void terminalCallback(struct limine_terminal* terminal, uint64_t, uint64_t,
-                      uint64_t, uint64_t)
-{
-    // TODO: terminalCallback
-    return;
-}
+static constexpr const uint32_t                DEFAULT_STACK_SIZE    = 65536;
 
-static volatile struct limine_bootloader_info_request bootloaderInfoRequest = {
+static volatile limine_bootloader_info_request bootloaderInfoRequest = {
     .id       = LIMINE_BOOTLOADER_INFO_REQUEST,
     .revision = 0,
 };
-static volatile struct limine_stack_size_request stackSizeRequest = {
+static volatile limine_stack_size_request stackSizeRequest = {
     .id         = LIMINE_STACK_SIZE_REQUEST,
     .revision   = 0,
     .stack_size = DEFAULT_STACK_SIZE,
 };
-static volatile struct limine_hhdm_request hhdmRequest = {
+static volatile limine_hhdm_request hhdmRequest = {
     .id       = LIMINE_HHDM_REQUEST,
     .revision = 0,
 };
-static volatile struct limine_terminal_request terminalRequest = {
-    .id       = LIMINE_TERMINAL_REQUEST,
-    .revision = 0,
-    .callback = terminalCallback,
-};
-static volatile struct limine_framebuffer_request framebufferRequest = {
+static volatile limine_framebuffer_request framebufferRequest = {
     .id       = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0,
 };
 #ifdef PH_USE_PML5
-static volatile struct limine_5_level_paging_request pml5Request = {
+static volatile limine_5_level_paging_request pml5Request = {
     .id       = LIMINE_5_LEVEL_PAGING_REQUEST,
     .revision = 0,
 };
 #endif
-// TODO: SMP
-static volatile struct limine_memmap_request memmapRequest = {
+/*static volatile limine_smp_request smpRequest = {
+    .id       = LIMINE_SMP_REQUEST,
+    .revision = 0,
+};*/
+static volatile limine_memmap_request memmapRequest = {
     .id       = LIMINE_MEMMAP_REQUEST,
     .revision = 0,
 };
-static volatile struct limine_kernel_file_request kernelFileRequest = {
+
+static volatile limine_entry_point_request entryPointRequest = {
+    .id       = LIMINE_ENTRY_POINT_REQUEST,
+    .revision = 0,
+    .entry    = BootInfo::Initialize,
+};
+static volatile limine_kernel_file_request kernelFileRequest = {
     .id       = LIMINE_KERNEL_FILE_REQUEST,
     .revision = 0,
 };
-static volatile struct limine_rsdp_request rsdpRequest = {
+static volatile limine_rsdp_request rsdpRequest = {
     .id       = LIMINE_RSDP_REQUEST,
     .revision = 0,
 };
-static volatile struct limine_boot_time_request bootTimeRequest = {
+static volatile limine_boot_time_request bootTimeRequest = {
     .id       = LIMINE_BOOT_TIME_REQUEST,
     .revision = 0,
 };
-static volatile struct limine_kernel_address_request kernelAddressRequest = {
+static volatile limine_kernel_address_request kernelAddressRequest = {
     .id       = LIMINE_KERNEL_ADDRESS_REQUEST,
     .revision = 0,
 };
@@ -66,20 +65,23 @@ static volatile struct limine_kernel_address_request kernelAddressRequest = {
 static Framebuffer      framebuffer         = {};
 static MemoryMapEntry** memoryMap           = nullptr;
 static uint64_t         memoryMapEntryCount = 0;
+
 namespace BootInfo
 {
-    bool Initialize()
+    extern "C" void Initialize()
     {
-        if (!terminalRequest.response
-            || terminalRequest.response->terminal_count < 1)
-            return false;
+        (void)stackSizeRequest.response;
+        (void)entryPointRequest.response;
+#ifdef PH_USE_PML5
+        (void)pml5Request.response;
+#endif
+
+        Logger::EnableE9Logging();
         if (!framebufferRequest.response
             || framebufferRequest.response->framebuffer_count < 1)
-            return false;
-
+            panic(__FILE__ "Boot: Failed to acquire the framebuffer!\n");
         limine_framebuffer* limineFramebuffer
             = framebufferRequest.response->framebuffers[0];
-
         framebuffer.base
             = reinterpret_cast<uintptr_t>(limineFramebuffer->address);
         framebuffer.width        = limineFramebuffer->width;
@@ -97,19 +99,18 @@ namespace BootInfo
         framebuffer.blueMaskSize   = limineFramebuffer->blue_mask_size;
         framebuffer.blueMaskShift  = limineFramebuffer->blue_mask_shift;
 
+        Terminal::Initialize();
+        Logger::EnableTerminalLogging();
+
         if (!memmapRequest.response || memmapRequest.response->entry_count == 0)
             panic("Failed to acquire limine memory map entries!");
 
         memoryMap           = (MemoryMapEntry**)memmapRequest.response->entries;
         memoryMapEntryCount = memmapRequest.response->entry_count;
-
-        return true;
+        LogInfo("Framebuffer Address: %#p\n", framebuffer.base);
+        __asm__ goto("jmp kernelStart");
     }
-    void TerminalWrite(const char* str, const uint64_t length)
-    {
-        terminalRequest.response->write(terminalRequest.response->terminals[0],
-                                        str, length);
-    }
+    uint64_t     GetHHDMOffset() { return hhdmRequest.response->offset; }
     Framebuffer* GetFramebuffer() { return &framebuffer; }
     const char*  GetBootloaderName()
     {
@@ -118,6 +119,10 @@ namespace BootInfo
     const char* GetBootloaderVersion()
     {
         return bootloaderInfoRequest.response->version;
+    }
+    uintptr_t GetRSDPAddress()
+    {
+        return reinterpret_cast<uintptr_t>(rsdpRequest.response->address);
     }
     uint64_t GetBootTime() { return bootTimeRequest.response->boot_time; }
     uint64_t GetKernelPhysicalAddress()
--- Kernel/BootInfo.hpp
+++ Kernel/BootInfo.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdint.h>
+#include "Common.hpp"
 
 static constexpr const uint32_t FRAMEBUFFER_MEMORY_MODEL_RGB = 0x01;
 
@@ -30,38 +30,45 @@ struct Framebuffer
     }
 };
 
-enum MemoryMapEntryType
-{
-    eUsable = 0,
-    eReserved,
-    eACPI_Reclaimable,
-    eACPI_NVS,
-    eBadMemory,
-    eBootloaderReclaimable,
-    eKernelAndModules,
-    eFramebuffer,
-};
+inline static constexpr const uint32_t MEMORY_MAP_USABLE                 = 0;
+inline static constexpr const uint32_t MEMORY_MAP_RESERVED               = 1;
+inline static constexpr const uint32_t MEMORY_MAP_ACPI_RECLAIMABLE       = 2;
+inline static constexpr const uint32_t MEMORY_MAP_ACPI_NVS               = 3;
+inline static constexpr const uint32_t MEMORY_MAP_BAD_MEMORY             = 4;
+inline static constexpr const uint32_t MEMORY_MAP_BOOTLOADER_RECLAIMABLE = 5;
+inline static constexpr const uint32_t MEMORY_MAP_KERNEL_AND_MODULES     = 6;
+inline static constexpr const uint32_t MEMORY_MAP_FRAMEBUFFER            = 7;
 
 struct MemoryMapEntry
 {
-    uintptr_t base;
-    uint64_t  length;
-    uint64_t  type;
+    uint64_t base;
+    uint64_t length;
+    uint64_t type;
 };
 
 using MemoryMap = MemoryMapEntry**;
 
+struct CPUInfo
+{
+    uint32_t  cpuID;
+    uint32_t  lapicID;
+    uint64_t  reserved;
+    uintptr_t gotoAddress;
+    uint64_t  extraArgument;
+};
+
 namespace BootInfo
 {
-    bool         Initialize();
-    void         TerminalWrite(const char* str, const uint64_t length);
+    extern "C" void Initialize();
 
-    Framebuffer* GetFramebuffer();
-    const char*  GetBootloaderName();
-    const char*  GetBootloaderVersion();
-    uint64_t     GetBootTime();
-    uint64_t     GetKernelPhysicalAddress();
-    uint64_t     GetKernelVirtualAddress();
-
-    MemoryMap    GetMemoryMap(uint64_t& entryCount);
+    const char*     GetBootloaderName();
+    const char*     GetBootloaderVersion();
+    uint64_t        GetHHDMOffset();
+    Framebuffer*    GetFramebuffer();
+    MemoryMap       GetMemoryMap(uint64_t& entryCount);
+    CPUInfo**       GetCPUInfos(uint32_t& cpuCount);
+    uintptr_t       GetRSDPAddress();
+    uint64_t        GetBootTime();
+    uint64_t        GetKernelPhysicalAddress();
+    uint64_t        GetKernelVirtualAddress();
 }; // namespace BootInfo
