6767737993bc1ad1f516440911b155695277c136
Vitriol1744 <vitriol1744@gmail.com>
2022-12-10 22:32:40 +0100

    Fix VMM

--- Kernel/Memory/VirtualMemoryManager.cpp
+++ Kernel/Memory/VirtualMemoryManager.cpp
@@ -6,8 +6,6 @@
 
 #include "Utility/KLibC.hpp"
 
-#include <stdbool.h>
-
 extern "C" symbol text_start_addr;
 extern "C" symbol text_end_addr;
 extern "C" symbol rodata_start_addr;
@@ -17,20 +15,18 @@ extern "C" symbol data_end_addr;
 
 #define PTE_ADDR_MASK       0x000ffffffffff000
 #define PTE_GET_ADDR(value) ((value)&PTE_ADDR_MASK)
-#define VMM_HIGHER_HALF     BootInfo::GetHHDMOffset()
 
-static PhysAddr* GetNextLevel(PhysAddr* level, uint64_t nextLevelIndex)
+static PhysAddr* GetNextLevel(uint64_t* level, uint64_t nextLevelIndex)
 {
     if ((level[nextLevelIndex] & PTE_PRESENT) != 0)
         return (uint64_t*)(PTE_GET_ADDR(level[nextLevelIndex])
-                           + VMM_HIGHER_HALF);
+                           + HIGHER_HALF_OFFSET);
 
-    PhysAddr* nextLevel   = (PhysAddr*)PhysicalMemoryManager::CallocatePages(1);
-    *nextLevel            = 8;
+    auto nextLevel   = reinterpret_cast<PhysAddr*>(PhysicalMemoryManager::CallocatePages(1));
 
-    level[nextLevelIndex] = (uint64_t)nextLevel | PTE_PRESENT | PTE_WRITABLE
-                          | PTE_EXECUTE_DISABLE;
-    return (PhysAddr*)((uint8_t*)nextLevel + VMM_HIGHER_HALF);
+    level[nextLevelIndex] = (uint64_t)nextLevel | (uint64_t)PTE_PRESENT
+                          | (uint64_t)PTE_WRITABLE | PTE_USER_ACCESSIBLE;
+    return (PhysAddr*)((uint8_t*)nextLevel + HIGHER_HALF_OFFSET);
 }
 
 #define PAGE_SIZE                   PhysicalMemoryManager::GetPageSize()
@@ -41,19 +37,21 @@ static PageMap kernelPageMap;
 
 #define PAGE_SIZE PhysicalMemoryManager::GetPageSize()
 
-void PageMap::MapPage(PhysAddr physAddr, VirtAddr virtAddr, uint64_t flags)
+void PageMap::MapPage(VirtAddr virtAddr, PhysAddr physAddr, uint64_t flags)
 {
     size_t    pml5Entry = (virtAddr >> 48) & 0x1ff;
-    size_t    pml4Entry = (virtAddr >> 39) & 0x1ff;
-    size_t    pml3Entry = (virtAddr >> 30) & 0x1ff;
-    size_t    pml2Entry = (virtAddr >> 21) & 0x1ff;
-    size_t    pml1Entry = (virtAddr >> 12) & 0x1ff;
+    size_t    pml4Entry = (virtAddr & (0x1ffull << 39)) >> 39;
+    size_t    pml3Entry = (virtAddr & (0x1ffull << 30)) >> 30;
+    size_t    pml2Entry = (virtAddr & (0x1ffull << 21)) >> 21;
+    size_t    pml1Entry = (virtAddr & (0x1ffull << 12)) >> 12;
 
-    PhysAddr* pml4      = reinterpret_cast<PhysAddr*>(topLevel);
+    auto pml4      = reinterpret_cast<PhysAddr*>(GetTopLevel());
     PhysAddr* pml3      = GetNextLevel(pml4, pml4Entry);
     PhysAddr* pml2      = GetNextLevel(pml3, pml3Entry);
     PhysAddr* pml1      = GetNextLevel(pml2, pml2Entry);
-    pml1[pml1Entry]     = physAddr | flags;
+
+    pml1[pml1Entry]     = physAddr | flags | PTE_USER_ACCESSIBLE;
+    pml1[pml1Entry] &= ~PTE_EXECUTE_DISABLE;
 }
 
 namespace VirtualMemoryManager
@@ -61,48 +59,66 @@ namespace VirtualMemoryManager
     void Initialize()
     {
         kernelPageMap.Create();
+        Assert(kernelPageMap.GetTopLevel() != 0);
 
         for (size_t i = 256; i < 512; i++)
             Assert(GetNextLevel((PhysAddr*)kernelPageMap.GetTopLevel(), i)
-                   != 0);
+                   != nullptr);
 
         for (uintptr_t addr = 0x1000; addr < 0x100000000; addr += PAGE_SIZE)
-            kernelPageMap.MapPage(addr, addr + VMM_HIGHER_HALF,
-                                  PTE_PRESENT | PTE_WRITABLE
-                                      | PTE_EXECUTE_DISABLE);
+        {
+            kernelPageMap.MapPage(addr, addr, PTE_PRESENT | PTE_WRITABLE);
+            kernelPageMap.MapPage(addr + HIGHER_HALF_OFFSET, addr,
+                                  PTE_PRESENT | PTE_WRITABLE);
+        }
+
+        for (auto p = AlignDown(reinterpret_cast<uintptr_t>(text_start_addr), PAGE_SIZE);
+             p < AlignUp(reinterpret_cast<uintptr_t>(text_end_addr), PAGE_SIZE);
+             p += PAGE_SIZE)
+        {
+            uintptr_t phys = p - BootInfo::GetKernelVirtualAddress()
+                           + BootInfo::GetKernelPhysicalAddress();
+
+            kernelPageMap.MapPage(p, phys,
+                                  PTE_PRESENT | PTE_USER_ACCESSIBLE);
+        }
+
+        for (auto p= AlignDown(reinterpret_cast<uintptr_t>(rodata_start_addr), PAGE_SIZE);
+             p < AlignUp(reinterpret_cast<uintptr_t>(rodata_end_addr), PAGE_SIZE);
+             p += PAGE_SIZE)
+        {
+            uintptr_t phys = p - BootInfo::GetKernelVirtualAddress()
+                           + BootInfo::GetKernelPhysicalAddress();
+            kernelPageMap.MapPage(p, phys, PTE_PRESENT);
+        }
+
+        for (auto p = AlignDown(reinterpret_cast<uintptr_t>(data_start_addr), PAGE_SIZE);
+             p < AlignUp(reinterpret_cast<uintptr_t>(data_end_addr), PAGE_SIZE);
+             p += PAGE_SIZE)
+        {
+            uintptr_t phys = p - BootInfo::GetKernelVirtualAddress()
+                           + BootInfo::GetKernelPhysicalAddress();
+            kernelPageMap.MapPage(p, phys, PTE_PRESENT | PTE_WRITABLE);
+        }
 
         uint64_t  entryCount = 0;
-        MemoryMap memmap     = BootInfo::GetMemoryMap(entryCount);
+        MemoryMap memoryMap     = BootInfo::GetMemoryMap(entryCount);
         for (size_t i = 0; i < entryCount; i++)
         {
-            MemoryMapEntry* entry = memmap[i];
+            MemoryMapEntry* entry = memoryMap[i];
 
             uintptr_t       base  = AlignDown(entry->base, PAGE_SIZE);
             uintptr_t top = AlignUp(entry->base + entry->length, PAGE_SIZE);
-            if (top <= 0x100000000) continue;
+             if (top <= 0x100000000) continue;
 
             for (uintptr_t j = base; j < top; j += PAGE_SIZE)
             {
-                if (j < 0x100000000) continue;
+                 if (j < 0x100000000) continue;
 
-                kernelPageMap.MapPage(j, j + VMM_HIGHER_HALF,
-                                      PTE_PRESENT | PTE_WRITABLE
-                                          | PTE_EXECUTE_DISABLE);
+                kernelPageMap.MapPage(j + HIGHER_HALF_OFFSET, j,
+                                      PTE_PRESENT | PTE_WRITABLE);
             }
         }
-        uint64_t kernelPhys = BootInfo::GetKernelPhysicalAddress();
-        uint64_t kernelVirt = BootInfo::GetKernelVirtualAddress();
-
-        for (PhysAddr p = AlignDown((PhysAddr)text_start_addr, PAGE_SIZE);
-             p < AlignUp((PhysAddr)text_end_addr, PAGE_SIZE); p += PAGE_SIZE)
-            kernelPageMap.MapPage(p - kernelVirt + kernelPhys, p, PTE_PRESENT);
-        for (PhysAddr p = AlignDown((PhysAddr)rodata_start_addr, PAGE_SIZE);
-             p < AlignUp((PhysAddr)rodata_end_addr, PAGE_SIZE); p += PAGE_SIZE)
-            kernelPageMap.MapPage(p - kernelVirt + kernelPhys, p, PTE_PRESENT);
-        for (PhysAddr p = AlignDown((PhysAddr)data_start_addr, PAGE_SIZE);
-             p < AlignUp((PhysAddr)data_end_addr, PAGE_SIZE); p += PAGE_SIZE)
-            kernelPageMap.MapPage(p - kernelVirt + kernelPhys, p,
-                                  PTE_PRESENT | PTE_WRITABLE);
 
         SwitchPageMap(kernelPageMap);
         LogInfo("Virtual Memory Manager Initialized");
@@ -110,10 +126,11 @@ namespace VirtualMemoryManager
 
     void SwitchPageMap(PageMap& pageMap)
     {
-        __asm__ __volatile__("mov %%cr3, %0"
-                             :
-                             : "r"(pageMap.GetTopLevel())
-                             : "memory");
+        __asm__ volatile(
+            "mov %0, %%cr3"
+            :
+            : "r"(pageMap.GetTopLevel() - HIGHER_HALF_OFFSET)
+            : "memory");
     }
 
     PageMap& GetKernelPageMap() { return kernelPageMap; }
--- Kernel/Memory/VirtualMemoryManager.hpp
+++ Kernel/Memory/VirtualMemoryManager.hpp
@@ -4,26 +4,25 @@
 
 #include "Memory/PhysicalMemoryManager.hpp"
 
+#define HIGHER_HALF_OFFSET BootInfo::GetHHDMOffset()
+
 using PhysAddr                                              = uintptr_t;
 using VirtAddr                                              = uintptr_t;
 
-inline static constexpr const uint32_t PTE_PRESENT          = BIT(0);
-inline static constexpr const uint32_t PTE_WRITABLE         = BIT(1);
-inline static constexpr const uint32_t PTE_USER_ACCESSIBLE  = BIT(2);
-inline static constexpr const uint32_t PTE_WRITE_THROUGH    = BIT(3);
-inline static constexpr const uint32_t PTE_CACHING_DISABLED = BIT(4);
-inline static constexpr const uint32_t PTE_ACCESSED         = BIT(5);
-inline static constexpr const uint32_t PTE_LARGE_PAGE       = BIT(7);
-inline static constexpr const uint32_t PTE_EXECUTE_DISABLE  = BIT(63);
+inline static constexpr const uint64_t PTE_PRESENT          = BIT(0);
+inline static constexpr const uint64_t PTE_WRITABLE         = BIT(1);
+inline static constexpr const uint64_t PTE_USER_ACCESSIBLE  = BIT(2);
+inline static constexpr const uint64_t PTE_WRITE_THROUGH    = BIT(3);
+inline static constexpr const uint64_t PTE_CACHING_DISABLED = BIT(4);
+inline static constexpr const uint64_t PTE_ACCESSED         = BIT(5);
+inline static constexpr const uint64_t PTE_LARGE_PAGE       = BIT(7);
+inline static constexpr const uint64_t PTE_EXECUTE_DISABLE
+    = 0x8000000000000000ULL;
 
 class PageMap
 {
   public:
     PageMap() = default;
-    explicit PageMap(uintptr_t topLevel)
-        : topLevel(topLevel)
-    {
-    }
 
     inline void Create()
     {
@@ -31,10 +30,10 @@ class PageMap
             PhysicalMemoryManager::CallocatePages(1));
     }
 
-    void        MapPage(PhysAddr physAddr, VirtAddr virtAddr, uint64_t flags);
+    void        MapPage(VirtAddr virtAddr, PhysAddr physAddr, uint64_t flags);
 
-    inline bool Exists() { return topLevel != 0; }
-    inline PhysAddr GetTopLevel() { return topLevel; }
+    [[nodiscard]] inline bool Exists() const { return topLevel != 0; }
+    [[nodiscard]] inline PhysAddr GetTopLevel() const { return topLevel + HIGHER_HALF_OFFSET; }
 
   private:
     PhysAddr topLevel = 0;
