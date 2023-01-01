e0f0147346e3b25efd3383d24a1190027b3d7f12
Vitriol1744 <vitriol1744@gmail.com>
2023-01-01 23:45:11 +0100

    Little cleanup

--- Kernel/Arch/x86/IOAPIC.cpp
+++ Kernel/Arch/x86/IOAPIC.cpp
@@ -1,12 +1,6 @@
 /*
- * Created by vitriol1744 on 11.12.2022.
+ * Created by vitriol1744 on 17.12.2022.
  * Copyright (c) 2022-2022, Szymon Zemke <Vitriol1744@gmail.com>
  *
  * SPDX-License-Identifier: GPL-3
  */
-#pragma once
-
-namespace VirtualFileSystem
-{
-
-}
