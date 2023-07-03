c38c1c071e4c36da567c61cec765c350b7ba4e7e
vitriol1744 <vitriol1744@gmail.com>
2023-07-03 17:02:34 +0200

    [feat]: Implement Math::DivRoundUp

--- Kernel/Utility/Math.hpp
+++ Kernel/Utility/Math.hpp
@@ -12,14 +12,18 @@ namespace Math
 {
     inline constexpr size_t AlignDown(size_t value, size_t alignment)
     {
-        return value  & ~(alignment - 1);
+        return value & ~(alignment - 1);
     }
     inline constexpr size_t AlignUp(size_t value, size_t alignment)
     {
         return AlignDown(value + alignment - 1, alignment);
     }
+    constexpr inline auto DivRoundUp(auto value, auto alignment)
+    {
+        return Math::AlignDown(value + alignment - 1, alignment) / alignment;
+    }
     inline constexpr bool IsPowerOfTwo(size_t value)
     {
         return value != 0 && !(value & (value - 1));
     }
-}
+} // namespace Math
