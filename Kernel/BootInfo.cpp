baf840abdb5387e9f1fb7384130cfd3740c8e2d7
vitriol1744 <vitriol1744@gmail.com>
2023-07-29 20:47:05 +0200

    [refactor]: Initialize global variables

--- Kernel/BootInfo.cpp
+++ Kernel/BootInfo.cpp
@@ -8,64 +8,104 @@
 
 #include "Common.hpp"
 
+namespace BootInfo
+{
+    extern "C" void Initialize();
+}
 static constexpr const uint32_t                DEFAULT_STACK_SIZE    = 65536;
 
 static volatile limine_bootloader_info_request bootloaderInfoRequest = {
     .id       = LIMINE_BOOTLOADER_INFO_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
 static volatile limine_stack_size_request stackSizeRequest = {
     .id         = LIMINE_STACK_SIZE_REQUEST,
     .revision   = 0,
+    .response   = nullptr,
     .stack_size = DEFAULT_STACK_SIZE,
 };
 static volatile limine_hhdm_request hhdmRequest = {
     .id       = LIMINE_HHDM_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
 static volatile limine_framebuffer_request framebufferRequest = {
     .id       = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
-/*static volatile limine_smp_request smpRequest = {
+static volatile limine_paging_mode_request pagingModeRequest = {
+    .id       = LIMINE_PAGING_MODE_REQUEST,
+    .revision = 0,
+    .response = nullptr,
+    .mode     = LIMINE_PAGING_MODE_DEFAULT,
+    .flags    = 0,
+};
+static volatile limine_smp_request smpRequest = {
     .id       = LIMINE_SMP_REQUEST,
     .revision = 0,
-};*/
+    .response = nullptr,
+    .flags    = 0,
+};
 static volatile limine_memmap_request memmapRequest = {
     .id       = LIMINE_MEMMAP_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
-
 static volatile limine_entry_point_request entryPointRequest = {
     .id       = LIMINE_ENTRY_POINT_REQUEST,
     .revision = 0,
+    .response = nullptr,
     .entry    = BootInfo::Initialize,
 };
 static volatile limine_kernel_file_request kernelFileRequest = {
     .id       = LIMINE_KERNEL_FILE_REQUEST,
     .revision = 0,
+    .response = nullptr,
+};
+static volatile limine_module_request moduleRequest = {
+    .id       = LIMINE_MODULE_REQUEST,
+    .revision = 0,
+    .response = nullptr,
 };
 static volatile limine_rsdp_request rsdpRequest = {
     .id       = LIMINE_RSDP_REQUEST,
     .revision = 0,
+    .response = nullptr,
+};
+static volatile limine_smbios_request smbiosRequest = {
+    .id       = LIMINE_SMBIOS_REQUEST,
+    .revision = 0,
+    .response = nullptr,
+};
+static volatile limine_efi_system_table_request efiSystemTableRequest = {
+    .id       = LIMINE_EFI_SYSTEM_TABLE_REQUEST,
+    .revision = 0,
+    .response = nullptr,
 };
 static volatile limine_boot_time_request bootTimeRequest = {
     .id       = LIMINE_BOOT_TIME_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
 static volatile limine_kernel_address_request kernelAddressRequest = {
     .id       = LIMINE_KERNEL_ADDRESS_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
-
-static volatile limine_module_request moduleRequest = {
-    .id       = LIMINE_MODULE_REQUEST,
+static volatile limine_dtb_request dtbRequest = {
+    .id       = LIMINE_DTB_REQUEST,
     .revision = 0,
+    .response = nullptr,
 };
 
-static Framebuffer      framebuffer         = {};
-static MemoryMapEntry** memoryMap           = nullptr;
-static uint64_t         memoryMapEntryCount = 0;
+namespace
+{
+    Framebuffer      framebuffer         = {};
+    MemoryMapEntry** memoryMap           = nullptr;
+    uint64_t         memoryMapEntryCount = 0;
+} // namespace
 
 extern "C" [[noreturn]]
 void kernelStart();
@@ -77,23 +117,10 @@ namespace BootInfo
         (void)stackSizeRequest.response;
         (void)entryPointRequest.response;
 
-        // Enable SSE
-#if PH_ARCH == PH_ARCH_X86_64
-        __asm__ volatile(
-            "mov %%cr0, %%rax\n"
-            "and $0xfffb, %%ax\n"
-            "or $0x2, %%ax\n"
-            "mov %%rax, %%cr0\n"
-            "mov %%cr4, %%rax\n"
-            "or $(3 << 9), %%ax\n"
-            "mov %%rax, %%cr4\n"
-            :);
-#endif
-
         Logger::EnableE9Logging();
         if (!framebufferRequest.response
             || framebufferRequest.response->framebuffer_count < 1)
-            Panic(__FILE__ "Boot: Failed to acquire the framebuffer!\n");
+            panic("Boot: Failed to acquire the framebuffer!");
         limine_framebuffer* limineFramebuffer
             = framebufferRequest.response->framebuffers[0];
         framebuffer.base
@@ -114,18 +141,15 @@ namespace BootInfo
         framebuffer.blueMaskShift  = limineFramebuffer->blue_mask_shift;
 
         if (!memmapRequest.response || memmapRequest.response->entry_count == 0)
-            Panic("Failed to acquire limine memory map entries!");
+            panic("Boot: Failed to acquire limine memory map entries");
 
         memoryMap = reinterpret_cast<MemoryMapEntry**>(
             memmapRequest.response->entries);
         memoryMapEntryCount = memmapRequest.response->entry_count;
 
         kernelStart();
-        //        __asm__ goto("jmp kernelStart");
     }
-    uint64_t     GetHHDMOffset() { return hhdmRequest.response->offset; }
-    Framebuffer* GetFramebuffer() { return &framebuffer; }
-    const char*  GetBootloaderName()
+    const char* GetBootloaderName()
     {
         return bootloaderInfoRequest.response->name;
     }
@@ -133,21 +157,10 @@ namespace BootInfo
     {
         return bootloaderInfoRequest.response->version;
     }
-    uintptr_t GetRSDPAddress()
-    {
-        return reinterpret_cast<uintptr_t>(rsdpRequest.response->address);
-    }
-    uint64_t GetBootTime() { return bootTimeRequest.response->boot_time; }
-    uint64_t GetKernelPhysicalAddress()
-    {
-        return kernelAddressRequest.response->physical_base;
-    }
-    uint64_t GetKernelVirtualAddress()
-    {
-        return kernelAddressRequest.response->virtual_base;
-    }
-
-    MemoryMap GetMemoryMap(uint64_t& entryCount)
+    uint64_t     GetHHDMOffset() { return hhdmRequest.response->offset; }
+    Framebuffer* GetFramebuffer() { return &framebuffer; }
+    limine_smp_response* GetSMP_Response() { return smpRequest.response; }
+    MemoryMap            GetMemoryMap(uint64_t& entryCount)
     {
         entryCount = memoryMapEntryCount;
         return memoryMap;
@@ -160,6 +173,20 @@ namespace BootInfo
                 return moduleRequest.response->modules[i];
         }
         return nullptr;
+    }
+    uintptr_t GetRSDPAddress()
+
+    {
+        return reinterpret_cast<uintptr_t>(rsdpRequest.response->address);
+    }
+    uint64_t GetBootTime() { return bootTimeRequest.response->boot_time; }
+    uint64_t GetKernelPhysicalAddress()
+    {
+        return kernelAddressRequest.response->physical_base;
+    }
+    uint64_t GetKernelVirtualAddress()
+    {
+        return kernelAddressRequest.response->virtual_base;
     }
 
 } // namespace BootInfo
